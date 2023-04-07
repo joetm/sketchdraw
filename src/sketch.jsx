47f1eb6928c2ffb91de649ab87afe82ecbbc1e1b
Jonas <jonaso@web.de>
2023-04-07 19:06:16 +0300

    replicate

--- src/sketch.jsx
+++ src/sketch.jsx
@@ -6,35 +6,73 @@ import React, { useState, useEffect, useRef } from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import Drawing, { brushArc } from 'react-drawing'
-
 import StatusPoller from './components/StatusPoller'
+
+// const replicate = new Replicate({token: process.env.REPLICATE_API_TOKEN})
 
 
 function Homepage() {
   const [status, setStatus] = useState('ready')
   const [brushColor, setBrushColor] = useState('rgba(10,10,10, 0.6)')
   const canvasRef = useRef(null)
+  const promptRef = useRef('')
 
-  const handleSend = (e) => {
+  async function handleSend(e) {
     e.preventDefault()
     e.stopPropagation()
+    const prompt = promptRef.current.value
+    console.log('prompt', prompt)
     const dataURL = canvasRef.current.toDataURL("image/jpeg", 1.0)
     console.log('dataURL', dataURL)
+    // let image = dataURL.split('data:image/png;')[1]
+    // console.log('image', image)
+
+    const a_prompt = 'best quality, extremely detailed' // default
+    const n_prompt = 'longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality' // default
+
+    // const controlnetModel = await replicate.models.get("jagilley/controlnet-scribble:435061a1b5a4c1e26740464bf786efdfa9cb3a3ac488595a2de23e143fdb0117")
+    // const controlnetPrediction = await controlnetModel.predict({
+    //   image: dataURL,
+    //   prompt: prompt,
+    //   image_resolution: '512',
+    //   ddim_steps: 50,
+    //   scale: 9, // guidance scale
+    //   eta: 0, // DDIM
+    //   a_prompt, // an added prompt
+    //   n_prompt, // negative prompts
+    // })
+
+    // TODO: move this to node server
+    const options = {  
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Origin': "*",
+        'Host': 'localhost',
+        'Authorization': `Token ${process.env.REPLICATE_API_TOKEN}`
+      },
+      body: JSON.stringify({
+        "version": "435061a1b5a4c1e26740464bf786efdfa9cb3a3ac488595a2de23e143fdb0117",
+        "input": {
+          image: dataURL,
+          prompt: prompt,
+          image_resolution: '512',
+          ddim_steps: 50,
+          scale: 9, // guidance scale
+          eta: 0, // DDIM
+          a_prompt, // an added prompt
+          n_prompt, // negative prompts
+        }
+      })
+    }
+
+    const output = await fetch('https://api.replicate.com/v1/predictions', options)
+      .then((res) => res.json())
+
+    console.log('output', output)
+
   }
-
-  // const handleMouseDown = (e) => {
-  //   canvasRef.current.onMouseDown(e);
-  // };
-  // const handleMouseMove = (e) => {
-  //   canvasRef.current.onMouseMove(e);
-  // };
-  // const handleMouseUp = (e) => {
-  //   canvasRef.current.onMouseUp(e);
-  // };
-
-  //             onMouseDown={handleMouseDown}
-  //             onMouseMove={handleMouseMove}
-  //             onMouseUp={handleMouseUp}
 
   return (
     <div className="App">
@@ -64,6 +102,7 @@ function Homepage() {
             Describe what you want to see
           </Form.Text>
           <Form.Control
+            ref={promptRef}
             id="prompt"
             as="textarea"
             rows={3} 
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-  root: './src',
-  build: {
-    outDir: '../dist',
-    minify: false,
-    emptyOutDir: true,
-  },
-});
+
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd())
+  return {
+    define: {
+      // 'process.env': env,
+      'process.env.VITE_REPLICATE_API_TOKEN': JSON.stringify(env.VITE_REPLICATE_API_TOKEN),
+    },
+    root: './src',
+    build: {
+      outDir: '../dist',
+      minify: false,
+      emptyOutDir: true,
+    },
+  }
+})
+
